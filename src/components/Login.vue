081468f4bda3d8bca0c8d252caebd991e5c3ce1e
杨治法 <1596510418@qq.com>
2020-08-17 20:26:48 +0800

    登录页面绘制1

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,31 @@
 <template>
-    $END$
+  <div class="login_container">
+      <div class="login_box">
+
+      </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: ""
-    }
+
+export default {
+  name: ''
+}
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+  .login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+  .login_box{
+    background-color: white;
+    width: 500px;
+    height: 300px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    border-radius: 3px;
+    margin: -150px 0 0 -250px;
+  }
 </style>
