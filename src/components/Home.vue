3054b39df73e24633c1c05dcbc036283d7eb1329
杨治法 <1596510418@qq.com>
2020-08-20 21:47:20 +0800

    首页布局绘制

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container>
+    <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+    <el-container class="home-container">
+      <el-aside width="200px">aside</el-aside>
+      <el-main>main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -19,5 +23,16 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.home-container {
+  height: 100vh;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Aside, Main, Header } from 'element-ui'
 
 Vue.prototype.$message = Message
 Vue.use(Button)
 Vue.use(FormItem)
 Vue.use(Form)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Header)
