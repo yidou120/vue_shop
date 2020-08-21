14af12f22224d49a1b35afa8538492957117eeba
杨治法 <1596510418@qq.com>
2020-08-21 22:34:06 +0800

    顶部导航栏设置

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <el-container>
-    <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+    <el-header>
+      <div>
+        <img src="../assets/heima.png"/>
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
     <el-container class="home-container">
       <el-aside width="200px">aside</el-aside>
       <el-main>main</el-main>
@@ -28,6 +34,19 @@ export default {
 }
 .el-header {
   background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 .el-aside {
   background-color: #333744;
