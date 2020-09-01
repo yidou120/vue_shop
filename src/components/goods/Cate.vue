c9bfdd8ccd68e12f1045dce76937ec487bc33cbc
杨治法 <1596510418@qq.com>
2020-09-01 22:44:15 +0800

    添加分类级联显示已完成

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -8,7 +8,7 @@
     <el-card>
       <el-row>
         <el-col>
-          <el-button type="primary">添加分类</el-button>
+          <el-button type="primary" @click="addCateDialog">添加分类</el-button>
         </el-col>
       </el-row>
       <tree-table :data="cateList"
@@ -44,6 +44,30 @@
         :total="total">
       </el-pagination>
     </el-card>
+    <el-dialog
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      >
+      <el-form :model="cateInfo" ref="cateInfoRef" :rules="cateInfoRules" label-width="100px">
+        <el-form-item label="分类名称：" prop="cat_name">
+          <el-input v-model="cateInfo.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级名称：">
+          <el-cascader
+            v-model="selectKeys"
+            :options="parentCateList"
+            :props="cascaderProps"
+            @change="handleChange"
+            clearable
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+      <span slot="footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCateDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -52,6 +76,26 @@ export default {
   name: '',
   data () {
     return {
+      parentCateList: [],
+      cascaderProps: {
+        expandTrigger: 'hover',
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children',
+        checkStrictly: true
+      },
+      selectKeys: [],
+      addCateDialogVisible: false,
+      cateInfo: {
+        cat_name: '',
+        cat_pid: '0',
+        cat_level: '0'
+      },
+      cateInfoRules: {
+        cat_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' }
+        ]
+      },
       queryInfo: {
         pagesize: 3,
         pagenum: 1
@@ -102,6 +146,22 @@ export default {
     handleCurrentChange (pageNum) {
       this.queryInfo.pagenum = pageNum
       this.getCateList()
+    },
+    addCateDialog () {
+      this.getParentCateList()
+      this.addCateDialogVisible = true
+    },
+    async getParentCateList () {
+      const { data: res } = await this.$http.get('categories', {
+        params: {
+          type: 2
+        }
+      })
+      if (res.meta.status !== 200) return this.$message.error('获取分类列表失败')
+      this.parentCateList = res.data
+    },
+    handleChange (selectKeys) {
+      console.log(selectKeys)
     }
   }
 }
@@ -110,5 +170,8 @@ export default {
 <style scoped lang="less">
 .treeTable {
   margin-top: 15px;
+}
+.el-cascader {
+  width: 100%;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Aside, Main, Header, Menu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Switch, Table, TableColumn, Dialog, MessageBox, Tag, Tree } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Aside, Main, Header, Menu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Switch, Table, TableColumn, Dialog, MessageBox, Tag, Tree, Cascader } from 'element-ui'
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
@@ -22,3 +22,4 @@ Vue.use(TableColumn)
 Vue.use(Dialog)
 Vue.use(Tag)
 Vue.use(Tree)
+Vue.use(Cascader)
