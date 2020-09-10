be2bb2af4070d105900984565ec367af375a4261
杨治法 <1596510418@qq.com>
2020-09-10 19:16:44 +0800

    商品列表页面绘制

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -1,10 +1,63 @@
 <template>
-    
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input
+            placeholder="请输入内容"
+            v-model="searchContent">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加商品</el-button>
+        </el-col>
+      </el-row>
+      <el-table
+        :data="goodsList"
+        border
+        style="width: 100%">
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column
+          prop="goods_name"
+          label="商品名称"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="goods_price"
+          label="商品价格(元)"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="goods_weight"
+          label="商品重量">
+        </el-table-column>
+        <el-table-column
+          prop="add_time"
+          label="创建时间">
+        </el-table-column>
+        <el-table-column
+          label="操作">
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: ''
+  name: '',
+  data () {
+    return {
+      searchContent: '',
+      goodsList: {}
+    }
+  }
 }
 </script>
 
