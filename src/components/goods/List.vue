553db78b520b5a4d640f1cbf6f62bfbb62a76e6b
杨治法 <1596510418@qq.com>
2020-09-11 21:17:48 +0800

    商品列表页面

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -26,12 +26,12 @@
         <el-table-column
           prop="goods_name"
           label="商品名称"
-          width="180">
+          width="700px">
         </el-table-column>
         <el-table-column
           prop="goods_price"
           label="商品价格(元)"
-          width="180">
+          >
         </el-table-column>
         <el-table-column
           prop="goods_weight"
@@ -39,12 +39,22 @@
         </el-table-column>
         <el-table-column
           prop="add_time"
-          label="创建时间">
+          label="创建时间"
+          width="150px">
         </el-table-column>
         <el-table-column
           label="操作">
         </el-table-column>
       </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -55,8 +65,33 @@ export default {
   data () {
     return {
       searchContent: '',
-      goodsList: {}
+      goodsList: [],
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      }
     }
+  },
+  async created () {
+    const { data: res } = await this.$http.get('goods', {
+      params: this.queryInfo
+    })
+    console.log(res)
+    if (res.meta.status !== 200) {
+      this.$message.error('商品列表加载失败')
+    }
+    res.data.goods.forEach(i => {
+      const date = new Date(i.add_time)
+      const Y = date.getFullYear() + '-'
+      const M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
+      const D = date.getDate() + ' '
+      const h = date.getHours() + ':'
+      const m = date.getMinutes() + ':'
+      const s = date.getSeconds()
+      i.add_time = Y + M + D + h + m + s
+    })
+    this.goodsList = res.data.goods
   }
 }
 </script>
