b43fcb69237b0039c4d9fe229cb712f977305727
杨治法 <1596510418@qq.com>
2020-09-18 09:07:02 +0800

    订单列表查询功能/物流信息查询功能

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,12 +1,141 @@
 <template>
   <div>
-    订单
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="orderInfo.query" clearable>
+            <el-button slot="append" icon="el-icon-search" @click="getOrderList"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+      <el-table
+        :data="orderList"
+        stripe
+        border
+        style="width: 100%">
+        <el-table-column
+          type="index"
+          label="#">
+        </el-table-column>
+        <el-table-column
+          prop="order_number"
+          label="订单编号">
+        </el-table-column>
+        <el-table-column
+          prop="order_price"
+          label="订单价格"
+          width="80px">
+        </el-table-column>
+        <el-table-column
+          prop="order_pay"
+          label="是否付款"
+          width="100px">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.order_pay==='0'" type="danger">未付款</el-tag>
+            <el-tag v-else="" type="danger">已付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="is_send"
+          label="是否发货"
+          width="80px">
+        </el-table-column>
+        <el-table-column
+          prop="create_time"
+          label="下单时间"
+          width="200px">
+          <template slot-scope="scope">
+            {{scope.row.create_time | timeformat}}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          width="150px">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" class="el-icon-edit"></el-button>
+            <el-button size="mini" type="success" class="el-icon-location" @click="showLocation(scope.row)"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="orderInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="orderInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+    </el-card>
+    <el-dialog
+      title="查看物流进度"
+      :visible.sync="locationDialogVisible"
+      width="50%">
+      <el-timeline :reverse="reverse">
+        <el-timeline-item
+          v-for="(activity, index) in activities"
+          :key="index"
+          :timestamp="activity.time">
+          {{activity.context}}
+        </el-timeline-item>
+      </el-timeline>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-  name: ''
+  name: '',
+  data () {
+    return {
+      activities: [],
+      reverse: false,
+      locationDialogVisible: false,
+      orderList: [],
+      orderInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      total: 0
+    }
+  },
+  created () {
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList () {
+      console.log(this.orderInfo.query)
+      const { data: res } = await this.$http.get('orders', {
+        params: this.orderInfo
+      })
+      if (res.meta.status !== 200) return this.$message.error('获取订单列表失败')
+      console.log(res)
+      this.orderList = res.data.goods
+      this.total = res.data.total
+    },
+    handleSizeChange (pagesize) {
+      this.orderInfo.pagesize = pagesize
+      this.getOrderList()
+    },
+    handleCurrentChange (pagenum) {
+      this.orderInfo.pagenum = pagenum
+      this.getOrderList()
+    },
+    async showLocation (row) {
+      const { data: res } = await this.$http.get('/kuaidi/1106975712662')
+      if (res.meta.status !== 200) return this.$message.error('获取物流信息失败')
+      this.activities = res.data
+      this.locationDialogVisible = true
+      console.log(res)
+      console.log(row)
+    }
+  }
 }
 </script>
 
